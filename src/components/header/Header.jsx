9cf6fbd226f98a2bf47e2e299f4b4039242798d6
fehim <fehimornek@gmail.com>
2022-03-21 14:22:04 +0300

    deployment

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,8 @@ import {BsChevronDoubleDown} from "react-icons/bs"
 const Header = () => {
   return (
     <div>
-      <div class='screen'>
-        <ul class='circles'>
+      <div className='screen'>
+        <ul className='circles'>
           <li></li>
           <li></li>
           <li></li>
@@ -19,12 +19,12 @@ const Header = () => {
           <li></li>
           <li></li>
         </ul>
-        <div class="context">
+        <div className="context">
           <h2>Welcome</h2>
           <h1>It's <b>Fehim</b></h1>
-          <h2 class>Your Friendly <b>Deep Learning</b> Engineer</h2>
+          <h2 className>Your Friendly <b>Deep Learning</b> Engineer</h2>
           <Socials/>
-          <a href='#about' class="scroll"><BsChevronDoubleDown/></a>
+          <a href='#about' className="scroll"><BsChevronDoubleDown/></a>
         </div>
       </div>
     </div>
